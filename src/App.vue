18d5e1870a769add1c6e1062e6fa2e2d9574aa2a
egdiala <dialaegwuchukwu@gmail.com>
2021-04-21 02:00:17 +0100

    added image assets

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,47 @@
 <template>
-  <div id="app">
-    <Header />
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app :style="{background: $vuetify.theme.themes.dark.background}">
+    <v-app-bar style="display: none;"></v-app-bar>
+
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+
+      <v-bottom-navigation app shift color="yellow" background-color="background">
+    <v-btn>
+      <span>Home</span>
+      <v-icon>mdi-home</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Booking</span>
+      <v-icon>mdi-account-cash</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Gallery</span>
+      <v-icon>mdi-file-image</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import Header from './components/Header.vue'
+import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
+
   components: {
     HelloWorld,
-    Header
-  }
-}
-</script>
+  },
 
+  data: () => ({
+    active: true,
+    value: 'home'
+  }),
+
+  mounted() {
+    this.$vuetify.theme.dark = true;
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Vuesax from 'vuesax'
-
-import 'vuesax/dist/vuesax.css' //Vuesax styles
-Vue.use(Vuesax)
+import vuetify from '@/plugins/vuetify'
 
 Vue.config.productionTip = false
 
 new Vue({
-    render: h => h(App),
+    vuetify,
+    render: h => h(App)
 }).$mount('#app')
